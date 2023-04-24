c4702fef4ef52e7a1a017c15a624564fb1c85f4d
Ares Lazarus <cernunos_kav@msn.com>
2023-04-24 18:00:53 +0300

    more helper funcs

--- SilenceFs/Common.fs
+++ SilenceFs/Common.fs
@@ -7,3 +7,4 @@ module Common =
     let sps (reader: AudioFileReader) =
         reader.WaveFormat.SampleRate * reader.WaveFormat.Channels
 
+    let byte2singleConversionFactor = int64 (sizeof<float32>/sizeof<byte>)
